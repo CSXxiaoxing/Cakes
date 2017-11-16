8f2c36f37ca71c9238cfe4d8c1d265cc708226f5
zhaojingjing <guzhao593@163.com>
2017-11-16 20:31:35 +0800

    上传doaction.php

---
+++ Reactapi/doAction.php
@@ -0,0 +1,9 @@
+<?php
+    header('Access-Control-Allow-Origin:*');
+    include 'server.php';
+    if (isset($_POST['upload'])) {
+      move_uploaded_file($_FILES['upfile']['tmp_name'], "../web/src/up_tmp/" . $_FILES['upfile']['name']);
+      echo "./src/up_tmp/" . $_FILES['upfile']['name'];
+      exit;
+    }
+?>
