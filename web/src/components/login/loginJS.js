80357f9762fc1b92634a7f3bee2a2ca4c3804bba
CSX <251156539@qq.com>
2017-11-16 21:33:41 +0800

    123

--- web/src/components/login/loginJS.js
+++ web/src/components/login/loginJS.js
@@ -4,7 +4,7 @@ $(window).resize(function(){
     var w = $('#LoginDL').outerWidth();
     var h = $('#LoginDL').outerHeight();
 
-    $('form').css({
+    $('#LoginDL form').css({
         'left': w/2,
         'top' : h/2,
         'transform': 'translate(-50%,-50%)'
