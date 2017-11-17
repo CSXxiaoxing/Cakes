8ddf7dd949c7e70b2715a0027000fe7af2c3b1a1
CSX <251156539@qq.com>
2017-11-17 13:30:49 +0800

    合并no.1

--- Reactapi/server.php
+++ Reactapi/server.php
@@ -3,33 +3,33 @@
 	/**
 	 * 连接数据库函数
 	 */
-	function connect(){
-		$severname = 'localhost';
-		$username = 'root';
-		$password = '123456';
-		$dbname = 'cake';
-		$conn = new mysqli($severname,$username,$password,$dbname);
-		if($conn->connect_error){
-		    die('连接失败：' . $conn->connect_error);
-		}
-		$conn->set_charset('utf8');
-		return $conn;
+	// function connect(){
+	// 	$severname = 'localhost';
+	// 	$username = 'root';
+	// 	$password = '123456';
+	// 	$dbname = 'cake';
+	// 	$conn = new mysqli($severname,$username,$password,$dbname);
+	// 	if($conn->connect_error){
+	// 	    die('连接失败：' . $conn->connect_error);
+	// 	}
+	// 	$conn->set_charset('utf8');
+	// 	return $conn;
 		
-	}
+	// }
     // 无密码测试
-    // function connect(){
-    //     $severname = 'localhost';
-    //     $username = 'root';
-    //     $password = '';
-    //     $dbname = 'cake';
-    //     $conn = new mysqli($severname,$username,$password,$dbname);
-    //     if($conn->connect_error){
-    //         die('连接失败：' . $conn->connect_error);
-    //     }
-    //     $conn->set_charset('utf8');
-    //     return $conn;
+    function connect(){
+        $severname = 'localhost';
+        $username = 'root';
+        $password = '';
+        $dbname = 'cake';
+        $conn = new mysqli($severname,$username,$password,$dbname);
+        if($conn->connect_error){
+            die('连接失败：' . $conn->connect_error);
+        }
+        $conn->set_charset('utf8');
+        return $conn;
         
-    // }
+    }
 	
 	//英德测试
 //	function connect(){
